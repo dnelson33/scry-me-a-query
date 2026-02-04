0d235aabd5391e947f5afa96554f6bb837590b08
Dustin Nelson <musician4christ26@gmail.com>
2026-02-04 08:32:59 -0600

    Added individual card layout Added emoji support for mana emojis Mapped additional card fields for new layout Fixed bug with border: queries

--- scryfall/service/types.py
+++ scryfall/service/types.py
@@ -3,6 +3,13 @@ from typing import TypedDict, List, Optional
 class ScryQueryCard(TypedDict):
     scryfall_url: str
     image_uri: str
+    name: str
+    mana_cost: Optional[str]
+    type_line: str
+    oracle_text: Optional[str]
+    price: Optional[float]
+    price_foil: Optional[float]
+    
 
 class PageInfo(TypedDict):
     page_number: int
